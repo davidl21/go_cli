89e4102a99e3831e0ce53eb8793e494b0b4ad45b
David Liu <dliu@esri.com>
2024-07-31 15:24:46 -0700

    setup main func and succesfully fetched data from weather api

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Weather struct {
+	Location struct {
+		Name string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+
+	Country struct {
+		Name string `json:"name"`
+		Country string `json:"country"`
+	} `json:"country"`
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	res, err := http.Get("")
+
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close();
+
+	if res.StatusCode != 200 {
+		panic("Error fetching data from Weather API.")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
